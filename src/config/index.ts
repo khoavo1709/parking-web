eeca38b58f1654f2ad8800c104b935f2f9d931f9
khoavo <khoavvd1709@gmail.com>
2024-04-16 12:13:03 +0700

    update dependence and create page template

--- src/config/index.ts
+++
@@ -1,5 +0,0 @@
-import MerchantRoutes from "./MerchantRoutes";
-import AppVariables from "./AppVariables";
-import MenuItems from "./MenuItems";
-
-export { MenuItems, MerchantRoutes, AppVariables };

cd0c3c5e13ae9441b4d4ddfeb483102520464649
Lloyd <lloyd@Lloyds-MacBook-Pro.local>
2024-05-06 10:05:36 +0700

    get timeframes details

--- parking-web/src/components/ParkingLots/TimeFrameDetails/index.tsx
+++ parking-web/src/components/ParkingLots/TimeFrameDetails/index.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 interface IProps {
   idParkingLot: string;
 }
-
 const TimeFrameDetails = (props: IProps) => {
-  return <div>{props.idParkingLot}</div>;
+  const [data, setData] = useState<Array<TimeFrame>>();
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:8088/api/merchant/time-frame/get-list?parkingLotId=${props.idParkingLot}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(res);
+      })
+      .then((res) => {
+        console.log(res.data?.data);
+        setData(res.data?.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [props.idParkingLot]);
+
+  return <div>{JSON.stringify(data)}</div>;
 };
 
 export default TimeFrameDetails;

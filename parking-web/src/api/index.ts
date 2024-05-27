33be7369bfb5c227196e4da78c57d509f19a720d
khoavo <khoavvd1709@gmail.com>
2024-05-27 12:27:13 +0700

    add api and action for company

--- parking-web/src/api/index.ts
+++ parking-web/src/api/index.ts
@@ -4,5 +4,14 @@ import parkingLotApi from "./parkingLotApi";
 import blockApi from "./blockApi";
 import timeFrameApi from "./timeFrameApi";
 import employeeApi from "./employeeApi";
+import companyApi from "./companyApi";
 
-export { authApi, userApi, parkingLotApi, blockApi, timeFrameApi, employeeApi };
+export {
+  authApi,
+  userApi,
+  parkingLotApi,
+  blockApi,
+  timeFrameApi,
+  employeeApi,
+  companyApi,
+};
